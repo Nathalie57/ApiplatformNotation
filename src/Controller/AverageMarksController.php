8901c2056714c17a1ac668d88e0257be9525c90c
Nathalie <nathaliehenrion@hotmail.com>
2020-07-26 21:27:14 +0200

    unless "use" deletion

--- src/Controller/AverageMarksController.php
+++ src/Controller/AverageMarksController.php
@@ -2,12 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Mark;
-use App\Services\calculateClassroomAverage;
-use App\Services\CalculateClassroomAverage as ServicesCalculateClassroomAverage;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-
 class AverageMarksController
 {
     public function __invoke($data)
